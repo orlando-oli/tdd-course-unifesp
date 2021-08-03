9dc4a00ef53e05ffcfa2c50baf81ab8d8d205c2c
Orlando Oliveira <oliorlando6@gmail.com>
2021-08-03 17:51:49 -0300

    refactor dollar to extend money

--- src/dollar.ts
+++ src/dollar.ts
@@ -1,11 +1,9 @@
-export class Dollar {
-  constructor(private readonly value: number) {}
+import { Money } from './money';
 
+class Dollar extends Money {
   multiplyBy(factor: number): Dollar {
     return new Dollar(this.value * factor);
   }
-
-  equals(anotherDollar: Dollar): boolean {
-    return anotherDollar.value === this.value;
-  }
 }
+
+export { Dollar };

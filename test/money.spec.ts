20ab751f57aeae5cd62d8ca71182d788cc1fee2a
Orlando Oliveira <oliorlando6@gmail.com>
2021-08-03 17:51:28 -0300

    replace dollar with money and add euros tests

---
+++ test/money.spec.ts
@@ -0,0 +1,24 @@
+import { Dollar } from '../src/dollar';
+import { Euro } from '../src/euro';
+
+describe('Money', () => {
+  it('should multiply dollars properly', () => {
+    const fiveDollars = new Dollar(5);
+
+    expect(fiveDollars.multiplyBy(2).equals(new Dollar(10))).toBeTruthy();
+    expect(fiveDollars.multiplyBy(3).equals(new Dollar(15))).toBeTruthy();
+  });
+  it('should multiply euros properly', () => {
+    const fiveEuros = new Euro(5);
+
+    expect(fiveEuros.multiplyBy(2).equals(new Euro(10))).toBeTruthy();
+    expect(fiveEuros.multiplyBy(3).equals(new Euro(15))).toBeTruthy();
+  });
+
+  it('Money.equals(): should return true if and only if Money compared share the same value property', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+    expect(new Euro(5).equals(new Euro(5))).toBeTruthy();
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+    expect(new Euro(5).equals(new Euro(6))).toBeFalsy();
+  });
+});
